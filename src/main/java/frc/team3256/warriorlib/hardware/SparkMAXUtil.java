67c1cbb3a34b3dfeca7dbc1337aa4b2691441032
Nic Pham <nicholas.pham@warriorlife.net>
2019-01-21 10:49:44 -0800

    Changed gradle version from 2019.1.1 to 2019.2.1 | Added RevRobotics dependency | Added SparkMAX Util class to hardware

---
+++ src/main/java/frc/team3256/warriorlib/hardware/SparkMAXUtil.java
@@ -0,0 +1,63 @@
+package frc.team3256.warriorlib.hardware;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+
+/**
+ * Utility class to generate SPARK MAX objects
+ */
+public class SparkMAXUtil {
+    /**
+     * How often the Spark's are updated
+     */
+    private static double controlLoopPeriod = 1.0/200.0;
+
+    private static CANSparkMax generateGenericSparkMAX (int id, CANSparkMaxLowLevel.MotorType type){
+        CANSparkMax sparkMax = new CANSparkMax(id, type);
+        sparkMax.setInverted(false);
+        return sparkMax;
+    }
+
+    private static CANSparkMax generateSlaveSparkMAX (int id, CANSparkMaxLowLevel.MotorType type, CANSparkMax masterSpark){
+        CANSparkMax sparkMax = new CANSparkMax(id, type);
+        sparkMax.follow(masterSpark);
+        sparkMax.setInverted(false);
+        return sparkMax;
+    }
+
+    public static void setCurrentLimit (int limit, CANSparkMax... sparks){
+        for (CANSparkMax sparkMax: sparks) {
+            sparkMax.setSmartCurrentLimit(limit);
+        }
+    }
+
+    public static void setCurrentLimit (int stallLimit, int freeLimit, CANSparkMax... sparks){
+        for (CANSparkMax sparkMax: sparks) {
+            sparkMax.setSmartCurrentLimit(stallLimit, freeLimit);
+        }
+    }
+
+    public static void setCurrentLimit (int stallLimit, int freeLimit, int rpmLimit, CANSparkMax... sparks){
+        for (CANSparkMax sparkMax: sparks) {
+            sparkMax.setSmartCurrentLimit(stallLimit, freeLimit, rpmLimit);
+        }
+    }
+
+    public static void setBrakeMode(CANSparkMax ... sparks) {
+        for (CANSparkMax sparkMax: sparks) {
+            sparkMax.setIdleMode(CANSparkMax.IdleMode.kBrake);
+        }
+    }
+
+    public static void setCoastMode(CANSparkMax ... sparks) {
+        for (CANSparkMax sparkMax: sparks) {
+            sparkMax.setIdleMode(CANSparkMax.IdleMode.kCoast);
+        }
+    }
+
+    public static void setRampRate (double seconds, CANSparkMax ... sparks) {
+        for (CANSparkMax sparkMax: sparks) {
+            sparkMax.setRampRate(seconds);
+        }
+    }
+}
