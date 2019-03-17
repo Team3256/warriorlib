b487c67a844f3cc22f8bf5265e069025c72455bc
ƝƚȻ <nicholascatpham@gmail.com>
2019-03-16 17:21:26 -0700

    Created H-ButterflyDrive branch, modified DrivePower

--- src/main/java/frc/team3256/warriorlib/control/DrivePower.java
+++ src/main/java/frc/team3256/warriorlib/control/DrivePower.java
@@ -1,26 +1,42 @@
 package frc.team3256.warriorlib.control;
 
 public class DrivePower {
-	private double left;
-	private double right;
+	private double leftX;
+	private double leftY;
+	private double rightX;
+	private double rightY;
 	private boolean highGear;
 
-	public DrivePower(double left, double right, boolean highGear) {
-		this.left = left;
-		this.right = right;
+	public DrivePower(double leftX, double leftY, double rightX, double rightY, boolean highGear) {
+		this.leftX = leftX;
+		this.leftY = leftY;
+		this.rightX = rightX;
+		this.rightY = rightY;
 		this.highGear = highGear;
 	}
 
-	public DrivePower(double left, double right) {
-		this(left, right, true);
+	public DrivePower(double leftY, double rightX, boolean highGear) {
+		this(0.0, leftY, rightX, 0.0, highGear);
 	}
 
-	public double getLeft() {
-		return left;
+	public DrivePower(double leftY, double rightX) {
+		this(0.0, leftY, rightX, 0.0, true);
 	}
 
-	public double getRight() {
-		return right;
+	public double getLeftX() {
+		return leftX;
+	}
+
+	public double getLeftY() {
+		return leftY;
+	}
+
+	public double getRightX() {
+		return rightX;
+	}
+
+	public double getRightY() {
+		return rightY;
 	}
 
 	public boolean getHighGear() {
